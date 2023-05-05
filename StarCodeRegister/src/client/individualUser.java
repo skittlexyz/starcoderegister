30806e271b140b6cab14c135a0829b866c18d180
Moisés Corrêa <54242235+skittlexyz@users.noreply.github.com>
2023-05-05 08:31:44 -0300

    Add files via upload

---
+++ StarCodeRegister/src/client/individualUser.java
@@ -0,0 +1,13 @@
+package client;
+
+public class individualUser {
+	
+	public String name;
+	public String email;
+	public long tel;
+	public long zipcode;
+	
+	public individualUser() {
+		
+	}
+}
---
+++ StarCodeRegister/src/main/main.java
@@ -0,0 +1,13 @@
+package main;
+
+import system.Register;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Register system = new Register();
+		system.launch();
+	}
+
+}
